da58d93fe3ae58178b38d292aaf66f59a0a03db3
Boris00 <Boris00>
2017-01-27 01:51:48 -0600

    Added the delete option without errorsa and other bugs fixed

--- chainSaw_Rec_Holder.py
+++ chainSaw_Rec_Holder.py
@@ -1,28 +1,31 @@
 import sqlite3
 
 db = sqlite3.connect('chainSawRecorHolder.db') # creates or opens database file
-cur = db.cursor() # Need a cursor objectand
+cur = db.cursor() # Need a cursor object and
 # Create a table if not exists...
-cur.execute('create table if not exists recordHolder (holder text, country text, number int)')
-
+cur.execute('create table if not exists recordHolder (fileId INTEGER PRIMARY KEY, holder text, country text, number int)')
 
 def choice():
-    print(''''What would you like to do:
+    options = '''What would you like to do:
         1. Add
-        2. Delete
-        3. Update
-        4. Exit
-        ''')
+        2. Show list
+        3. Delete
+        4. Update
+        5. Exit
+        '''     # This is printed to have the user select an option.
 
-    choice = input('Enter your selection: ')
     while True:
+        print(options)
+        choice = input('Enter your selection: ')
         if choice == '1':
-            addHolders()
+            addHolders() # Add new record holder function
         elif choice == '2':
+            show_list() # Displays the list function
+        elif choice == '3':
             delete_option()
-        elif choice == '4':
+        elif choice == '5':
             break
-            quit() # Closes the db
+            # quit() # Closes the db
 
 
 def addHolders():
@@ -32,60 +35,69 @@ def addHolders():
 
     # The program will ask the user if it wants to add another record holder.
     while True:
-        holder = input('Enter the name of the holder: ')
+        fileId = 'NULL'
+        holder = input('Enter the name of the record holder: ')
         country = input('Enter the country: ')
-        while True:
+        while True: # This while loop is for the int that is needed for number of times
             try:
                 number = int(input('Enter number of catches: '))
                 break # This breaks ones the user has enterd an integer
             except Exception as e:
                 continue
-        allHoldes.append((str(holder), str(country), int(number)))
+        # This appends the items as tuple to the array list.
+        allHoldes.append((None, str(holder), str(country), int(number)))
         resp = input('Do you want to add another record holder to the list Y/N: ')
-        if resp == 'Y'.lower():
-            continue
-        else:     # If the user enter 'y' the program loops again
+        if resp != 'Y'.lower(): # Checks for response to break the loop
             break
-
-    # Use of parameters ? as place holders
-    # cur.execute('insert into recordHolder values (?, ?, ?)', (holder, country, number))
+             # If the user enter 'y' the program loops again
 
     # This line uses the array allHolders add to db with executemany.
-    cur.executemany('insert into recordHolder values (?, ?, ?)', (allHoldes))
+    cur.executemany('insert into recordHolder values (?, ?, ?, ?)', (allHoldes))
 
     # Fetch and display
     cur.execute('select * from recordHolder')
 
     # for loop
-    print('Name,   Country,  Number of Catcher')
+    print_rows() # Calls funcion to print headers
     for row in cur:
         print(row)
 
     db.commit() # Saves changes
-    db.close() # Closes the db
+
+
 
 def delete_option():
     cur.execute('select * from recordHolder')
-    print("Opened database successfully")
-    for row in cur:
-         print(row)
-    # name = input('Enter name of holder: ')
-    cur.execute("delete * from recordHolder")
-    db.commit
-
-
-    # cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
-    # for row in cursor:
-    #     print row
-    #
+    print_rows() # Prints headers for the columns and the values below
+    for value in cur:
+        print(value)
+    # The user is asked to enter the id from the list.
+    enterId = int(input('Enter the "ID" of the raw that you want deleted: '))
+    delete_raw = 'DELETE FROM recordHolder WHERE fileId=?' # This deletes the by id
+    cur.execute(delete_raw, (enterId,)) # Executes the delete action
     print("Operation done successfully")
-    db.close()
+    db.commit() # saves data
 
-def quit():
-    db.close()
+
+
+
+def show_list():
+    '''This display the list of items stored'''
+    cur.execute('select * from recordHolder')
+    print('This is your list:')
+    print_rows() # Prints headers for the columns and the values below
+    for value in cur:
+        print(value)
+
+def print_rows():
+    # Prints headers for the columns and the values below
+    return print('ID, Name, Country, Number of Catcher')
+
 
 def main():
     choice()
+    db.commit()
+    db.close()
 
 if __name__ == '__main__':
         main()
